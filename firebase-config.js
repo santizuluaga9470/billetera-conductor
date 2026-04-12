fa12e9f6f629e54726720bed4dabfd3c6c90ef77
santizuluaga9470 <santizuluaga94@gmail.com>
2026-04-12 18:00:34 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js — Reemplaza con tus datos reales de Firebase Console
 window.FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyDdayuT50DQqa8Vgq13TNZpQwCAw2zoYoU",
-  authDomain:        "carro-alquiler.firebaseapp.com",
-  projectId:         "carro-alquiler",
-  storageBucket:     "carro-alquiler.firebasestorage.app",
-  messagingSenderId: "877660757092",
-  appId:             "1:877660757092:web:6190cb03093f9eba1a5371"
+  apiKey:            "AIzaSyCs1fQm2EjCa2bFGs-S4ODnq46xqT1Avjw",
+  authDomain:        "billetera-conductor.firebaseapp.com",
+  projectId:         "billetera-conductor",
+  storageBucket:     "billetera-conductor.firebasestorage.app",
+  messagingSenderId: "182427840795",
+  appId:             "1:182427840795:web:257a1ba4d9a4c473e263be"
 };
